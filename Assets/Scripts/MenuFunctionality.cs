276cd454611c8afc9a3862f973d8d88621e99515
NHutsenko <dlink223@gmail.com>
2018-08-06 17:42:30 +0300

    added main menu

---
+++ Assets/Scripts/MenuFunctionality.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctionality : MonoBehaviour {
+
+    public void PlayGame() {
+        SceneManager.LoadScene("DefenderPlayStyle");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
